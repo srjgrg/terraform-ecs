16a6302ce13b04b2ddd30cee23f7f51f5a36763c
srjgrg <srjlamichane@gmail.com>
2020-02-26 20:20:44 -0500

    edited

--- asg.tf
+++ asg.tf
@@ -1,9 +1,4 @@
-iam.tf				terraform-helloapp
-main.tf				terraform.tfstate
-sgs.tf				terraform.tfstate.backup
-terraform-ecs
-Suraj@macs-mbp-7 terraform1 % cd terraform-helloapp 
-Suraj@macs-mbp-7 terraform-helloapp % cat asg.tf 
+ 
 resource "aws_autoscaling_group" "default" {
   name                      = "terraform-ecs-nginx"
   max_size                  = 1
@@ -14,6 +9,5 @@ resource "aws_autoscaling_group" "default" {
   force_delete              = true
   launch_configuration      = "${aws_launch_configuration.default.name}"
   vpc_zone_identifier       = ["subnet-122b865f", "subnet-799f8125"]
-  target_group_arns         = ["${aws_lb_target_group.default.arn}"]
+ # target_group_arns         = ["${aws_lb_target_group.default.arn}"]
 }
-
