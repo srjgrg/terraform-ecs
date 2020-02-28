56682d5d2735f7abd635ae49c3b07752c2cb63f3
srjgrg <srjlamichane@gmail.com>
2020-02-27 19:14:11 -0500

    add load balancer

---
+++ alb.tf
@@ -0,0 +1,29 @@
+resource "aws_lb" "default" {
+  name               = "terraform-ecs-nginx"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.default_lb.id}"]
+  subnets            = ["subnet-122b865f", "subnet-799f8125"]
+  enable_deletion_protection = true
+  tags = {
+    Environment = "dev"
+  }
+}
+
+resource "aws_lb_listener" "default" {
+  load_balancer_arn = "${aws_lb.default.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.default.arn}"
+  }
+}
+
+resource "aws_lb_target_group" "default" {
+  name     = "terraform-ecs-ngnix"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-62567f18"
+}
+
