52ddda9f6eddb2c0ba8ac6eaf2778cd93f5f9084
srjgrg <srjlamichane@gmail.com>
2020-02-27 19:20:16 -0500

    added security groups

---
+++ alb-sgs.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "default_lb" {
+  name        = "terraform-ecs-nginx-lb"
+  description = "Allow traffic"
+  vpc_id      = "vpc-62567f18"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
